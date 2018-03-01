3b59791fc70fb14f0b2fd2bd5c186e84fd43b17b
Eduardo Mauricio Mueller Nicastro <edu.nicastro@gmail.com>
2018-03-01 14:59:04 -0300

    Created the first todo reducer manually

--- src/index.js
+++ src/index.js
@@ -5,20 +5,31 @@ import './index.css';
 import expect, { createSpy, spyOn, isSpy } from 'expect';
 var deepFreeze = require('deep-freeze');
 
+const todo = (state, action) => {
+	switch (action.type) {
+		case 'ADD_TODO':
+			return {
+				id: action.id,
+				text: action.text,
+				completed: false
+			};
+		case 'TOGGLE_TODO':
+			if (state.id !== action.id) {
+				return state;
+			}
+			return {
+				...state,
+				completed: !state.completed
+			};
+	}
+};
+
 const todos = (state = [], action) => {
 	switch (action.type) {
 		case 'ADD_TODO':
-			return [...state, { id: action.id, text: action.text, completed: false }];
+			return [...state, todo(undefined, action)];
 		case 'TOGGLE_TODO':
-			return state.map(todo => {
-				if (todo.id !== action.id) {
-					return todo;
-				}
-				return {
-					...todo,
-					completed: !todo.completed
-				};
-			});
+			return state.map(t => todo(t, action));
 		default:
 			return state;
 	}
